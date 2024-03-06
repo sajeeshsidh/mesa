038203368130b883325fc61d7c62001cffaf955a
Friedrich Vock <friedrich.vock@gmx.de>
2024-03-06 13:27:17 +0100

    aco: Add call info

--- src/amd/compiler/aco_ir.h
+++ src/amd/compiler/aco_ir.h
@@ -2316,6 +2316,10 @@ public:
    /* For shader part with previous shader part that has lds access. */
    bool pending_lds_access = false;
 
+   ABI callee_abi = {};
+   unsigned short arg_sgpr_count;
+   unsigned short arg_vgpr_count;
+
    struct {
       monotonic_buffer_resource memory;
       /* live temps out per block */
