c0f0f6c3e4fb072ef6bfefb4774a11bf5862855a
Friedrich Vock <friedrich.vock@gmx.de>
2024-05-04 16:01:59 +0200

    nir: Allow forward-declaring nir_parameter

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -3527,7 +3527,7 @@ nir_cf_list_is_empty_block(struct exec_list *cf_list)
    return false;
 }
 
-typedef struct {
+typedef struct nir_parameter {
    uint8_t num_components;
    uint8_t bit_size;
    /* True if this parameter is a deref used for returning values */
