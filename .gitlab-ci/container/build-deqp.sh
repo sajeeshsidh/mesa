eccaab9791b81534e361f458928c23c5561cf021
Connor Abbott <cwabbott0@gmail.com>
2024-06-18 08:02:05 -0400

    ci: Uprev VK-CTS to 1.3.8.3
    And backport fixes necessary for VK_KHR_dynamic_rendering_local_read on
    turnip.

--- .gitlab-ci/container/build-deqp.sh
+++ .gitlab-ci/container/build-deqp.sh
@@ -16,7 +16,7 @@ set -ex -o pipefail
 # - the GL release produces `glcts`, and
 # - the GLES release produces `deqp-gles*` and `deqp-egl`
 
-DEQP_VK_VERSION=1.3.8.2
+DEQP_VK_VERSION=1.3.8.3
 DEQP_GL_VERSION=4.6.4.1
 DEQP_GLES_VERSION=3.2.10.1
 
@@ -28,14 +28,17 @@ DEQP_GLES_VERSION=3.2.10.1
 
 # shellcheck disable=SC2034
 vk_cts_commits_to_backport=(
-    # Fix more ASAN errors due to missing virtual destructors
-    dd40bcfef1b4035ea55480b6fd4d884447120768
-
-    # Remove "unused shader stages" tests
-    7dac86c6bbd15dec91d7d9a98cd6dd57c11092a7
-
     # Emit point size from "many indirect draws" test
     771e56d1c4d03e073ddb7f1200ad6d57e0a0c979
+
+    # Fix depth/stencil input attachment mapping in VK_KHR_dynamic_rendering_local_read tests
+    b9306e076b5b027ed1e938aba8c58a2192ed5ace
+
+    # Fix input attachment mapping in VK_KHR_dynamic_rendering_local_read tests even more
+    bce68dc0cce49adc2ffcfedb9f415760a0fabb52
+
+    # Various fixes including use-after-free for VK_KHR_dynamic_rendering_local_read
+    28cffb571fd7cd63251c98b06a2c2a005e95ae3d
 )
 
 # shellcheck disable=SC2034
