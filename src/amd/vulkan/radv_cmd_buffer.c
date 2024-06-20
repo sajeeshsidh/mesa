2f146fc1487d247f1e072ce9c2e2fdd54355626d
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2024-06-20 17:48:49 +0200

    radv: add support for executing the DGC ACE IB
    It's disabled for now.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- src/amd/vulkan/radv_cmd_buffer.c
+++ src/amd/vulkan/radv_cmd_buffer.c
@@ -11505,14 +11505,26 @@ static void radv_dgc_after_dispatch(struct radv_cmd_buffer *cmd_buffer);
 static void
 radv_cmd_execute_ib(struct radv_cmd_buffer *cmd_buffer, const VkGeneratedCommandsInfoNV *pGeneratedCommandsInfo)
 {
+   VK_FROM_HANDLE(radv_indirect_command_layout, layout, pGeneratedCommandsInfo->indirectCommandsLayout);
    VK_FROM_HANDLE(radv_buffer, prep_buffer, pGeneratedCommandsInfo->preprocessBuffer);
+   VK_FROM_HANDLE(radv_pipeline, pipeline, pGeneratedCommandsInfo->pipeline);
    struct radv_device *device = radv_cmd_buffer_device(cmd_buffer);
+   const bool has_task_shader = layout->pipeline_bind_point == VK_PIPELINE_BIND_POINT_GRAPHICS &&
+                                pipeline->shaders[MESA_SHADER_TASK] && false /* TODO: Enable when fully implemented */;
 
    const uint64_t ib_offset = prep_buffer->offset + pGeneratedCommandsInfo->preprocessOffset;
    const uint32_t cmdbuf_size = radv_get_indirect_cmdbuf_size(pGeneratedCommandsInfo);
    struct radeon_winsys_bo *ib_bo = prep_buffer->bo;
 
    device->ws->cs_execute_ib(cmd_buffer->cs, ib_bo, ib_offset, cmdbuf_size >> 2, cmd_buffer->state.predicating);
+
+   if (has_task_shader) {
+      const uint32_t ace_cmdbuf_size = radv_get_indirect_ace_cmdbuf_size(pGeneratedCommandsInfo);
+
+      assert(cmd_buffer->gang.cs);
+      device->ws->cs_execute_ib(cmd_buffer->gang.cs, ib_bo, ib_offset + cmdbuf_size, ace_cmdbuf_size >> 2,
+                                cmd_buffer->state.predicating);
+   }
 }
 
 VKAPI_ATTR void VKAPI_CALL
