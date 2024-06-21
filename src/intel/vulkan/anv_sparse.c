alexvorxx/zink-xlib-termux

commit 18d8c3ca339d346b9d531f49c343353ac7a61c89

anv: Add missing ANV_BO_ALLOC_INTERNAL
Some places doing driver internal allocations was not setting
ANV_BO_ALLOC_INTERNAL, so adding the flag in those places here.

This will increase the accuracy of the RMV report.

Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28677>

src/intel/vulkan/anv_sparse.c
@@ -308,7 +308,9 @@ trtt_make_page_table_bo(struct anv_device *device, struct anv_bo **bo)
    struct anv_trtt *trtt = &device->trtt;
 
    result = anv_device_alloc_bo(device, "trtt-page-table",
-                                ANV_TRTT_PAGE_TABLE_BO_SIZE, 0, 0, bo);
+                                ANV_TRTT_PAGE_TABLE_BO_SIZE |
+                                ANV_BO_ALLOC_INTERNAL,
+                                0, 0, bo);
    if (result != VK_SUCCESS)
       return result;
 

src/intel/vulkan/anv_bo_sync.c
@@ -46,7 +46,8 @@ anv_bo_sync_init(struct vk_device *vk_device,
 
    return anv_device_alloc_bo(device, "bo-sync", 4096,
                               ANV_BO_ALLOC_EXTERNAL |
-                              ANV_BO_ALLOC_IMPLICIT_SYNC,
+                              ANV_BO_ALLOC_IMPLICIT_SYNC |
+                              ANV_BO_ALLOC_INTERNAL,
                               0 /* explicit_address */,
                               &sync->bo);
 }

src/intel/vulkan/anv_cmd_buffer.c
@@ -465,7 +465,7 @@ anv_cmd_buffer_set_ray_query_buffer(struct anv_cmd_buffer *cmd_buffer,
          struct anv_bo *new_bo;
          VkResult result = anv_device_alloc_bo(device, "RT queries shadow",
                                                ray_shadow_size,
-                                               0, /* alloc_flags */
+                                               ANV_BO_ALLOC_INTERNAL, /* alloc_flags */
                                                0, /* explicit_address */
                                                &new_bo);
          if (result != VK_SUCCESS) {
@@ -1457,7 +1457,7 @@ void anv_CmdSetRayTracingPipelineStackSizeKHR(
       struct anv_bo *new_bo;
       VkResult result = anv_device_alloc_bo(device, "RT scratch",
                                             rt->scratch.layout.total_size,
-                                            0, /* alloc_flags */
+                                            ANV_BO_ALLOC_INTERNAL, /* alloc_flags */
                                             0, /* explicit_address */
                                             &new_bo);
       if (result != VK_SUCCESS) {

src/intel/vulkan/anv_measure.c
@@ -94,7 +94,9 @@ anv_measure_init(struct anv_cmd_buffer *cmd_buffer)
    ASSERTED VkResult result =
       anv_device_alloc_bo(device, "measure data",
                           config->batch_size * sizeof(uint64_t),
-                          ANV_BO_ALLOC_MAPPED | ANV_BO_ALLOC_HOST_CACHED_COHERENT,
+                          ANV_BO_ALLOC_MAPPED |
+                          ANV_BO_ALLOC_HOST_CACHED_COHERENT |
+                          ANV_BO_ALLOC_INTERNAL,
                           0,
                           (struct anv_bo**)&measure->bo);
    measure->base.timestamps = measure->bo->map;

sajeeshsidh/mesa

commit 1bb13fe84d26dafc640d301015761606467adb44

anv/sparse: fix TR-TT page table bo size and flags
Since commit 18d8c3ca339d we were allocating a little more than what
we were actually using (2621440 bytes instead of 2097152, aka 0x280000
instead of 0x200000), and we were not properly marking the BO as
internal. No applications should be misbehaving because of this.

Fixes: 18d8c3ca339d ("anv: Add missing ANV_BO_ALLOC_INTERNAL")
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>

src/intel/vulkan/anv_sparse.c
@@ -335,9 +335,9 @@ trtt_make_page_table_bo(struct anv_device *device, struct anv_bo **bo)
    struct anv_trtt *trtt = &device->trtt;
 
    result = anv_device_alloc_bo(device, "trtt-page-table",
-                                ANV_TRTT_PAGE_TABLE_BO_SIZE |
+                                ANV_TRTT_PAGE_TABLE_BO_SIZE,
                                 ANV_BO_ALLOC_INTERNAL,
-                                0, 0, bo);
+                                0 /* explicit_address */, bo);
    if (result != VK_SUCCESS)
       return result;
 